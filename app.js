8036d58b9477e2bf636ef25854c70fbd2faf10ab
Dennys Hess <dhess@MAC-11-61.local>
2018-07-31 13:51:50 +0200

    updates to npm

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function init(){
             console.error( "Could not list the directory.", err );
             process.exit( 1 );
         }
-        
+
         // loop all files within directory
         files.forEach( function( file, index ) {
 
@@ -31,7 +31,7 @@ function init(){
                     console.error( "Error stating file.", error );
                     return;
                 }
-                
+
                 // send file path to format File function
             fs.readFile(readDir+file,'utf8', addToDataset);
             });
@@ -41,7 +41,7 @@ function init(){
 
 /**
  * Get Date in new format
- * @param {string} file 
+ * @param {string} file
  */
 function extractDate( date ){
     var time = date.slice(28, 33)+":00";
@@ -56,8 +56,8 @@ function extractDate( date ){
 
 /**
  * Formating the file to fit into the new textfile
- * @param {obj} err 
- * @param {string} content 
+ * @param {obj} err
+ * @param {string} content
  */
 function addToDataset(err, content){
     // do Formating
@@ -66,20 +66,20 @@ function addToDataset(err, content){
         if( !dataSet[dailyRecord[0][i]] ){
             console.log( dailyRecord[0][i] )
            // dataSet[dailyRecord[0][i]] = new Array();
-        }else{  
-             console.log( dailyRecord[0][i] ) 
+        }else{
+             console.log( dailyRecord[0][i] )
             //dataSet[dailyRecord[0][i]]
         }
         console.log('-----')
-    } 
+    }
     console.log( dailyRecord )
-    //fs.appendFileSync(dataFile+''+dailyRecord[1]+'.'+outputFormat, dailyRecord[0]);    
+    //fs.appendFileSync(dataFile+''+dailyRecord[1]+'.'+outputFormat, dailyRecord[0]);
 }
 
 
 /**
  * Convert to JSON Format for easy access to data points
- * @param {string} csv 
+ * @param {string} csv
  * @return {array}
  */
 function formatData(csv){
@@ -91,13 +91,13 @@ function formatData(csv){
     /*for(var h = 0; h<headers.length; h++){
         result[headers[h]] = {}
     }*/
-   
+
     var obj = {};
-    
+
     // Loop lines (stations)
     for(var i = 2; i < lines.length; i++) {
         var currentline = lines[i].split(",");
-        
+
         // Loop through data points within line (station)
         for( var j = 1; j<headers.length; j++ ){
             if( !obj[headers[j]] )
@@ -108,5 +108,5 @@ function formatData(csv){
     }
 
     //return result; //JavaScript object
-    return [obj, date]; // JSON, Time/Date 
+    return [obj, date]; // JSON, Time/Date
 }
